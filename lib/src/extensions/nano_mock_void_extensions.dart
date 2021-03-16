58d6be89ba64b248ebaae4a94e0a349cdb8ff353
Samuel Utbult <samuel.utbult@oborgen.se>
2021-03-16 14:37:38 +0100

    Change two references in lib/src/extensions/nano_mock_void_extensions.dart to remove warnings

--- lib/src/extensions/nano_mock_void_extensions.dart
+++ lib/src/extensions/nano_mock_void_extensions.dart
@@ -6,10 +6,10 @@ extension NanoMockVoidExtensions on NanoMock<void> {
   /// A `void` reference.
   static final void _void = (() {})();
 
-  /// Adds a mock to the [NanoMock] instance, which makes it so if the instance
+  /// Adds a mock to the `NanoMock` instance, which makes it so if the instance
   /// is called with `arguments`, the instance will return `void`.
   ///
-  /// The returned [NanoMockCallCount] can be used to access how many times the
+  /// The returned `NanoMockCallCount` can be used to access how many times the
   /// mock has been called.
   NanoMockCallCount whenVoid(List<dynamic> arguments) {
     // Call when with the given arguments and a void reference.
